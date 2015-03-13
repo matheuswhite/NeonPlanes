63bbd1412f908a9718a93d222cdcd6ab2b42b2cf
Matheus Tenorio <matheustenorio@ic.com.br>
2015-03-13 10:13:06 -0300

    Add debug and release configs

---
+++ NeonPlanes/TestMain.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+
+//C:\Users\Matheus\Documents\Codigos\Cplusplus\NeonPlanes\Debug\Images
+
+#ifndef _DEBUG
+std::string IMAGE_PATH = "Images\\";
+#else
+std::string IMAGE_PATH = "..\\Debug\\Images\\";
+#endif // !_DEBUG
+
+
+int main(int argc, char** argv) {
+	bool quit = false;
+
+	SDL_Event event;
+
+	IMAGE_PATH.append("pict3159.jpg");
+
+	SDL_Init(SDL_INIT_VIDEO);
+	IMG_Init(IMG_INIT_JPG);
+
+	SDL_Window * window = SDL_CreateWindow("SDL2 Displaying Image",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+	SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_Surface * image = IMG_Load(IMAGE_PATH.c_str());
+	SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, image);
+
+	while (!quit) {
+		SDL_WaitEvent(&event);
+
+		switch (event.type) {
+		case SDL_QUIT:
+			quit = true;
+			break;
+		}
+
+		SDL_RenderCopy(renderer, texture, NULL, NULL);
+		SDL_RenderPresent(renderer);
+	}
+
+	SDL_DestroyTexture(texture);
+	SDL_FreeSurface(image);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	IMG_Quit();
+	SDL_Quit();
+
+	return 0;
+}
