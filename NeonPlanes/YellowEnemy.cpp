f72f3278015b5039fe67a3c69c8e78f7c7a543b8
Matheus Tenorio <matheustenorio@ic.com.br>
2015-05-07 19:53:25 -0300

    size fix

--- NeonPlanes/YellowEnemy.cpp
+++ NeonPlanes/YellowEnemy.cpp
@@ -2,7 +2,7 @@
 
 YellowEnemy::YellowEnemy(std::string name) : Enemy(name)
 {
-	this->addComponent(new Rectangle(Vector2D(100, 200), Vector2D(64, 75), 0, "rectDestiny"));
+	this->addComponent(new Rectangle(Vector2D(100, 200), Vector2D(54, 55), 0, "rectDestiny"));
 
 	this->addSprite(new StaticSprite((Rectangle*)this->getComponent("rectDestiny"), "YellowEnemy.png", "yellow"));
 }
