e82dc10e5af8ea4a12b5dcfe199b7299dd500559
Matheus Tenorio <matheustenorio@ic.com.br>
2015-06-14 12:56:51 -0300

    change player velocity

--- NeonPlanes/Player.cpp
+++ NeonPlanes/Player.cpp
@@ -2,7 +2,7 @@
 
 Player::Player(std::string name) : Airplane(name)
 {
-	this->velocity = 3;
+	this->velocity = 4;
 	this->addComponent(new Rectangle(Vector2D(200,250),Vector2D(64,45),"destiny"));
 
 	this->addSprite(new StaticSprite((Rectangle*)this->getComponent("destiny"), "player.png", "StaticSprite"));
