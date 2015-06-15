acefe71e637a1d3e370805ae3570989ab648e372
Matheus Tenorio <matheustenorio@ic.com.br>
2015-06-14 21:02:37 -0300

    some things repaired in base_AI class

--- NeonPlanes/Base_AI.h
+++ NeonPlanes/Base_AI.h
@@ -9,11 +9,17 @@ public:
 	Base_AI(GameObject* enemy, std::string name) : GameObject(name), enemy(enemy)
 	{ 
 		this->enemyBehaviors = enemy->getVectorBaheviors();
+		auto initBehavior = enemy->getBehavior("InitialEnemyBehavior");
+		((InitialEnemyBehavior*)initBehavior)->setFlagInitialize(&this->flagInitialize);
+
+		this->createTimers();
 	}
 	virtual ~Base_AI() {}
 
+	virtual void createTimers() = 0;
 	virtual void manageBehaviors() = 0;
 protected:
+	bool flagInitialize;
 	GameObject* enemy;
 	std::vector<Behavior*> enemyBehaviors;
 	std::vector<SlaveTimer*> timers;
